56421856ac50e44cae24086f48c13d9ba8333c0e
Jeronimo <jeronimocp@ucit.ar>
2022-12-29 20:04:11 -0300

    me re colgue con los comit , pero bueno ahora no hacia falta

---
+++ README.md
@@ -0,0 +1,49 @@
+# CoderHouseJavaScript
+
+## Segunda entrega
+
+### Deberás entregar la estructura del proyecto, las variables de JS necesarias y los objetos de JS, correspondientes a la segunda entrega de tu proyecto final.
+
+### Codificar la funcionalidad inicial del simulador.
+
+### Identificar el flujo de trabajo del script en términos de captura de entradas ingresadas por el usuario, procesamiento esencial del simulador y notificación de resultados en forma de salida.
+
+### Página HTML y código fuente en JavaScript. Debe identificar el apellido del alumno/a en el nombre de archivo comprimido por “PreEntrega2+Apellido”.
+
+### resolver la entrega y subirla.
+
+## Objetivos específicos
+
+### Consigna
+
+- Capturar entradas mediante prompt().
+- Declarar variables y objetos necesarios para simular el proceso seleccionado.
+- Crear funciones y/o métodos para realizar operaciones (suma, resta, concatenación, división, porcentaje, etc).
+- Efectuar una salida, que es el resultado de los datos procesados, la cual puede hacerse por alert() o console.log().
+
+### Sugerencias
+
+Si bien, por el momento solo podemos hacer entradas con prompt() y salidas con alert() o console.log(), es suficiente para empezar a pensar el proceso a simular en términos de entradas, variables, estructuras, funciones, métodos y salidas. Verificar Rúbrica
+ones.
+
+# Se debe entregar
+
+- Estructura HTML del proyecto. -> OK
+- Variables de JS necesarias. -> OK
+- Funciones esenciales del proceso a simular. -> OK
+- Objetos de JS. -> OK
+- Arrays. -> OK
+- Métodos de búsqueda y filtrado sobre el Array. -> OK
+
+### Para tener en cuenta
+
+- La estructura hace referencia a el html y css, correspondientes al armado de la página general, pero que el JS que se evalúa, aún no está interactuando con ella.
+
+### Ejemplo en uso
+
+- Calcular costo total de productos y/o servicios seleccionados por el usuario.
+- Calcular pagos en cuotas sobre un monto determinado.
+- Calcular valor final de un producto seleccionado en función de impuestos y descuentos.
+- Calcular tiempo de espera promedio en relación con la cantidad de turnos registrados.
+- Calcular edad promedio de personas registradas.
+- Calcular nota final de alumnos ingresados.
---
+++ js.js
@@ -0,0 +1,180 @@
+
+
+//VARIABLES GLOBALES YA SE QUE HYA VARIABLE QUE NO SE USAN AHORA Y MUCHOS ARRAY VACIOS SON ARA MAS ADELANTA LAS USO DE GUIA 
+let nomUser = "";
+let monto = 0;
+let tasa = 50;
+let moneda = "Pesos";
+const IVA = 21;
+let cuotas = [1, 3, 6, 12, 24, 36];
+let cuotasa = [];
+let cuotaSinIva = [];
+let cuotasFinal = [];
+let cuotasSelec = 0;
+let totalC = [];
+let paquete = "";
+let cuotasSuma = 0;
+
+// SOLICITA MONTO PARA COTIZAR
+monto = document.querySelector("#monto_solicitado");
+paquete = document.querySelector("#paquete");
+cuotasSelec = document.querySelector("#plazo");
+
+let solicitud = document.querySelector(".simuladorContent");
+let botoneraSimulador = document.querySelector("#botoneraSimulador");
+
+// DIVIDE EL MONTO POR LA CUOTAS Y LES AGREGA LA TASA, CREA EL OBJETO Y LO GUARDA EN LOCALSTORAGE
+
+botoneraSimulador.addEventListener("click", function () {
+
+ 
+
+  paquete = document.querySelector("#paquete").value;
+  monto = document.querySelector("#monto_solicitado").value;
+  cuotasSelec = document.querySelector("#plazo").value;
+
+
+  let cantidadCuotas = cuotasSelec;
+  let pagoMes = monto / cantidadCuotas;
+  rest = (pagoMes * tasa) / 100;
+  pagoMes = pagoMes + rest;
+  let detalleCuotas = document.querySelector("#detalleCuotas");
+  detalleCuotas.innerHTML = `
+  <div class="alert alert-black" roler="alert">
+  <h4> 
+  En ${cuotasSelec} Cuotas de $${pagoMes} .- Pesos
+  </h4>
+  </div>
+  `;
+
+  //OBJETO PARA ARMAR EL PAQUETE ACEPTADO
+  function Prestamo() {
+    this.montoSolicitado = monto;
+    this.tasa = tasa;
+    this.moneda = moneda;
+    this.iva = IVA;
+    this.cuotas = cuotasSelec;
+    this.pagoPorMes = pagoMes;
+  }
+  //OBJETO PARA ARMAR EL PAQUETE ACEPTADO
+
+  // NUEVO OBJETO A PARTIR DEL  EL PRESTAMO
+
+  const prestamo1 = new Prestamo(
+    monto,
+    tasa,
+    moneda,
+    IVA,
+    cuotasSelec,
+    pagoMes
+  );
+ 
+  //GUARDADO DEL OBJETO EN LOCAL STORAGE
+  const prestamosOBJ = JSON.stringify(prestamo1);
+  localStorage.setItem("prestamo1", prestamosOBJ);
+
+  });
+// DIVIDE EL MONTO POR LA CUOTAS Y LES AGREGA LA TASA, CREA EL OBJETO Y LO GUARDA EN LOCALSTORAGE
+
+
+//RECUPERA LOS DATOS DEL LOCALSTORAGE PARA USARLOS
+const prestamo1 = JSON.parse(localStorage.getItem("prestamo1"));
+
+//MUESTRA EN EL NAV EL ULTIMO MONTO GUARDADO EN LOCAL STORAGE
+ultimoMontoSolicitado = document.querySelector("#ultimoMontoSolicitado");
+
+ultimoMontoSolicitado.innerHTML = `
+  
+  <div class="alert alert-dark" ">
+  <h6>
+  Ùltimo monto solicitado ${prestamo1.montoSolicitado}
+
+  </h6>
+  
+  
+  
+  </div>
+  
+  
+  `;
+//MUESTRA EN EL NAV EL ULTIMO MONTO GUARDADO EN LOCAL STORAGE
+
+//VARIABLES PARA EN SUBMIT
+let nomUserF = document.querySelector("#nameClient");
+let emailClient = document.querySelector("#emailClient");
+let infoNombre = document.querySelector(".infoNombre");
+let infoEmail = document.querySelector(".infoEmail");
+
+nomUserF.addEventListener("input", function () {
+  //VERIFICA QUE NO HAYA CAMPOS VACIOS Y SI LO HAY MUESTRA EL MENSAJE DEBAJO DEL INPUT
+  if (nomUserF.value === "") {
+    infoNombre.innerHTML = `
+  
+    <div class="alert alert-dark" role="alert">
+    <h5>
+    Debes poner un Nombre
+    </h5>
+    
+    
+    </div>
+    
+    
+    `;
+  }
+});
+emailClient.addEventListener("input", function () {
+  //VERIFICA QUE NO HAYA CAMPOS VACIOS Y SI LO HAY MUESTRA EL MENSAJE DEBAJO DEL INPUT
+  if (emailClient.value === "") {
+    infoEmail.innerHTML = `
+  
+    <div class="alert alert-dark" role="alert">
+    <h5>
+    Debes poner un correo valido
+    </h5>
+    
+    
+    </div>
+    
+    
+    `;
+  }
+});
+
+let formularioClient = document.querySelector("#formularioClient");
+let info = document.querySelector(".info");
+
+//monstrar formulario
+
+const printInfo = formularioClient.addEventListener("submit", function (e) {
+  e.preventDefault();
+  info.innerHTML = `
+  
+  <div class="alert alert-dark" ">
+  <h4>
+  Excelente ${nomUserF.value} los datos se enviaron correctamente.
+   Datos del préstamo: 
+ <lo>
+   <li>Monto Solicitado = $${prestamo1.montoSolicitado}.-</li>
+   <li>Tasa: ${prestamo1.tasa}%</li>
+   
+   <li>A pagar por mes cuotas de = ${prestamo1.pagoPorMes}.-</li>
+   <li>Cantidad de cuotas: ${prestamo1.cuotas}</li>
+   
+ </lo>
+  </h4>
+  
+  
+  
+  </div>
+  
+  
+  `;
+});
+
+//BOTON PARA RECARGAR LA PAGINA PARA PODER SOLICITAR OTRO CALCULO
+let refresh = document.querySelector('#refresh');
+refresh.addEventListener('click', _ => {
+            location.reload();
+});
+
+
